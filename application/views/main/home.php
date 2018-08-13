7e6f1c9b0502e057197be22ed2e2fe27645ef7d5
BillyOsvaldo <bill.jodh@gmail.com>
2018-08-13 14:40:51 +0700

    add page about

--- application/views/main/home.php
+++ application/views/main/home.php
@@ -62,7 +62,7 @@ form input[type=text] {float:left;width:80%;}
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li><a href="/index.php/home/about">Link</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
